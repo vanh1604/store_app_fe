ac151f5a5dfa16ac9a01bb2b99f29f637aef93a7
vanh1604 <VietAnhScout@gmail.com>
2025-11-27 19:41:37 +0700

    feat: coding cart screen

--- lib/views/screens/nav_screens/cart_screen.dart
+++ lib/views/screens/nav_screens/cart_screen.dart
@@ -1,10 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vanh_store_app/provider/cart_provider.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends ConsumerStatefulWidget {
   const CartScreen({super.key});
 
   @override
+  ConsumerState<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends ConsumerState<CartScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Cart screen')));
+    final cartData = ref.watch(cartProvider);
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          MediaQuery.of(context).size.height * 0.2,
+        ),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 118,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/icons/cartb.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 322,
+                top: 52,
+                child: Stack(
+                  children: [
+                    Image.asset('assets/icons/not.png', width: 25, height: 25),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+
+                      child: Container(
+                        width: 20,
+                        height: 20,
+                        padding: EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: Colors.yellow.shade800,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Center(
+                          child: Text(
+                            cartData.length.toString(),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 61,
+                top: 51,
+                child: Text(
+                  'My Cart',
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: cartData.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    textAlign: TextAlign.center,
+                    'Cart is empty \n Add some products to your cart',
+                    style: GoogleFonts.roboto(fontSize: 15, letterSpacing: 1.7),
+                  ),
+                  TextButton(onPressed: () {}, child: Text('SHOP NOW')),
+                ],
+              ),
+            )
+          : SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 49,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(color: Colors.grey.shade200),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        Positioned(
+                          left: 0,
+                          top: 0,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 49,
+
+                            decoration: BoxDecoration(color: Color(0xFFD7DDFF)),
+                          ),
+                        ),
+                        Positioned(
+                          left: 44,
+                          top: 19,
+                          child: Container(
+                            width: 10,
+                            height: 10,
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 69,
+                          top: 10,
+                          child: Text(
+                            'You Have ${cartData.length} items in your cart',
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1.7,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
   }
 }
